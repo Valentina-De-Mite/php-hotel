220fef56698fecf891ed9c961bfebc0e7988a054
Valentina De Mite <valesakura97@gmail.com>
2023-10-10 17:17:35 +0200

    printed hotels

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +54,28 @@
     <title>Hotels</title>
 </head>
 <body>
+
+<div class="container">
+
+<ul>
+    <?php foreach ($hotels as $hotel) : ?>
+        <li><?php echo $hotel["name"] ?></li>
+        <li><?php echo $hotel["description"] ?></li>
+        <li><?php echo $hotel["parking"] ?></li>
+        <li><?php echo $hotel["vote"] ?></li>
+        <li><?php echo $hotel["distance_to_center"] ?></li>
+  
+  
+  
+  
+   <?php endforeach; ?>
+</ul>
+    
+
+
+
+</div>
+
     
 </body>
 </html>
